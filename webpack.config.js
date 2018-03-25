22f92217370d06cb68cdce5b57a5c776fcfe3967
grindypeach <vdimitrov13@yahoo.com>
2018-03-25 13:53:18 +0300

    yarn setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+module.exports = {
+   entry: path.resolve(__dirname, 'src', 'index.jsx'),
+   output: {
+      path: path.resolve(__dirname, 'output'),
+      filename: 'bundle.js'
+   },
+   resolve: {
+      extensions: ['.js', '.jsx']
+   },
+   module: {
+      rules: [
+         {
+             test: /\.jsx/,
+             use: {
+                loader: 'babel-loader',
+                options: { presets: ['react', 'es2015'] }
+             }
+         },
+         {
+            test: /\.scss/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+         }
+      ]
+   },
+    devServer: {
+        contentBase: './src',
+        publicPath: '/output'
+    }
+};
